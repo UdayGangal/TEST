75c62aa086a9548e670567e2fd90f1b0b75a0ac5
UdayGangal <udaygangal2006@gmail.com>
2025-07-29 11:04:13 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
-int main()
-{
-    printf("Heloo");
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, choice;
+
+    
+    printf("Enter your first number: ");
+    scanf("%d", &a);
+
+    printf("Enter your second number: ");
+    scanf("%d", &b);
+
+    
+    printf("Enter 1 for addition, 2 for multiplication: ");
+    scanf("%d", &choice);
+
+   
+    switch(choice) {
+        case 1:
+            c = a + b;
+            printf("Sum = %d\n", c);
+            break;  
+
+        case 2:
+            d = a * b;
+            printf("Product = %d\n", d);
+            break;
+
+        default:
+            printf("Invalid choice.\n");
+    }
+
+    return 0;
 }
